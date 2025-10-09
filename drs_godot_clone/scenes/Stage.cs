b7a10549fdbe676599857993e26cae0ce4a93b13
Elias Kristoffer Grønn Moen <ekmoen@uia.no>
2025-10-09 14:49:20 +0200

    Add hitbox for stage and note logic

--- drs_godot_clone/scenes/Stage.cs
+++ drs_godot_clone/scenes/Stage.cs
@@ -1,12 +1,28 @@
 using Godot;
+using System.Collections.Generic;
 namespace Game.Stage
 {
     public partial class Stage : Sprite2D
     {
+        int score = 0;
+        [Export] float footXTolerance = 15f;
+        [Export] float stepTime = 0.2f;
+        [Export] float keyboardMovementSpeed = 69f;
         [Export] Foot foot0;
         [Export] Foot foot1;
-        [Export] float stepTime;
-        [Export] float speed = 69f;
+        [Export] Area2D hitbox;
+
+        List<Note.Note> notes = new();
+        public override void _Ready()
+        {
+            hitbox.AreaEntered += AddActiveNote;
+        }
+        
+
+        private void AddActiveNote(Node2D body)
+        {
+            if (body is Note.Note note) notes.Add(note);
+        }
 
         public void MoveFeet(InputData data)
         {
@@ -29,8 +45,8 @@ namespace Game.Stage
             float foot0Movement = Input.GetActionStrength("wasd_right") - Input.GetActionStrength("wasd_left");
             float foot1Movement = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
 
-            foot0.Position += ClampedPos(foot0Movement * (float)delta * speed);
-            foot1.Position += ClampedPos(foot1Movement * (float)delta * speed);
+            foot0.Position += ClampedPos(foot0Movement * (float)delta * keyboardMovementSpeed);
+            foot1.Position += ClampedPos(foot1Movement * (float)delta * keyboardMovementSpeed);
             if (Input.GetActionStrength("wasd_up") > 0f)
             {
                 Step(foot0, stepTime);
@@ -57,8 +73,69 @@ namespace Game.Stage
         private void Step(Foot foot, float time)
         {
             foot.Step(time);
+            foreach (Note.Note note in notes)
+            {
+                if (Mathf.Abs(note.GlobalPosition.X - foot.GlobalPosition.X) < footXTolerance)
+                {
+                    
+                    float yDistance = Mathf.Abs(note.GlobalPosition.Y - GlobalPosition.Y);
+                    int timing = TimingRating(yDistance);
+                    if (timing > 0) 
+                    {
+                        GivePoints(timing);
+                        NoteVFX(timing);
+                        notes.Remove(note);
+                        note.QueueFree();
+                    }
+                    
+                }
+            }
 
         }
+#region NoteHit
+        private void NoteVFX(int timing)
+        {
+            GD.Print("Wow, pretty effects!");
+        }
+
+        private void GivePoints(int timing)
+        {
+            score += timing * 10;
+            switch (timing)
+            {
+                case 3:
+                    GD.Print("PERFECT!!");
+                    break;
+                case 2:
+                    GD.Print("Great!");
+                    break;
+                default:
+                    GD.Print("OK");
+                    break;
+            }
+            GD.Print("Score: "+ score);
+        }
+
+        private int TimingRating(float yDistance)
+        {
+            int howGood = 3;
+            switch (yDistance)
+            {
+                case var _ when yDistance < 10f:  //perfect
+                    howGood = 3;
+                    break;
+                case var _ when yDistance < 25f: //good
+                    howGood = 2;
+                    break;
+                case var _ when yDistance < 45f: // ok
+                    howGood = 1;
+                    break;
+                default:                         // near miss
+                    break;
+            }
+            return howGood;
+        }
+#endregion NoteHit
         private void Unstep(Foot foot)
         {
             foot.Unstep();
