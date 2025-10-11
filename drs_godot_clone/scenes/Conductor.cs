da1f53464341d5beed430edd9f43782ac9ce28c4
Aristidis Akritidis <aristidisa@uia.no>
2025-10-11 11:59:40 +0200

    synced the conductor better to music

--- drs_godot_clone/scenes/Conductor.cs
+++ drs_godot_clone/scenes/Conductor.cs
@@ -1,82 +1,102 @@
 using Godot;
 using System;
-
-public partial class Conductor : AudioStreamPlayer2D
+namespace Game
 {
-    [Export] int BPM = 120;
-    [Export] int measures = 4;
-    [Export] private Timer offsetTimer;
-
-    float songPosition = 0.0f;
-    int songPositionInBeats = 1;
-    int crotchet;
-    int lastReportedBeat = 0;
-    int beatsBeforeStart = 0;
-    int measure = 1;
-
-
-    [Signal]
-    public delegate void BeatEventHandler(int position);
-    [Signal]
-    public delegate void MeasureEventHandler(int position);
-
-    public override void _Ready()
+    public partial class Conductor : AudioStreamPlayer2D
     {
-        this.crotchet = 60 / BPM;
-    }
+        [Export] float BPM = 120f;
+        [Export] int measures = 4;
+        [Export] private Timer offsetTimer;
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (Playing)
+        public static Conductor instance { get; private set; }
+
+        double songPosition = 0.0d;
+        int songPositionInBeats = 0;
+        float crotchet;
+        double lastBeatTime = 0.0d;
+        int lastReportedBeat = 0;
+        int beatsBeforeStart = 0;
+        int measure = 1;
+
+
+        [Signal]
+        public delegate void BeatEventHandler(int position);
+        [Signal]
+        public delegate void MeasureEventHandler(int position);
+
+        public override void _Ready()
         {
-            songPosition = GetPlaybackPosition() + (float)AudioServer.GetTimeSinceLastMix() - (float)AudioServer.GetOutputLatency();
-            songPositionInBeats = (int)Math.Floor(songPosition / crotchet) + beatsBeforeStart;
+            offsetTimer.Timeout += OnOffsetTimerTimeout;
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                QueueFree();
+            }
+            this.crotchet = 60 / BPM;
+            Console.WriteLine("Ready");
+        }
+
+        public override void _PhysicsProcess(double delta)
+        {
+            if (Playing)
+            {
+                songPosition += GetPlaybackPosition() + AudioServer.GetTimeSinceLastMix() - songPosition;
+                songPosition -= AudioServer.GetOutputLatency();
+                songPositionInBeats = (int)Math.Floor(songPosition / crotchet) + beatsBeforeStart;
+                
+                ReportBeat();
+            }
+        }
+
+        public void ReportBeat()
+        {
+            if (songPosition > lastBeatTime + crotchet)
+            {
+                if (measure > measures)
+                {
+                    measure = 1;
+                }
+                
+                EmitSignal(SignalName.Beat, songPositionInBeats);
+                EmitSignal(SignalName.Measure, measure);
+                lastBeatTime += crotchet;
+                measure += 1;
+                
+            }
+            
+        }
+
+        public void BeatOffset(int num)
+        {
+            this.beatsBeforeStart = num;
+            offsetTimer.WaitTime = crotchet;
+            offsetTimer.Start();
+            Console.WriteLine("BeatOffset function");
+        }
+
+        public void OnOffsetTimerTimeout()
+        {
+            this.songPositionInBeats += 1;
+            if (this.songPositionInBeats < this.beatsBeforeStart - 1)
+            {
+                offsetTimer.Start();
+            }
+            else if (this.songPositionInBeats == this.beatsBeforeStart - 1)
+            {
+                offsetTimer.WaitTime = offsetTimer.WaitTime -
+                                        (AudioServer.GetTimeToNextMix() + AudioServer.GetOutputLatency());
+                offsetTimer.Start();
+            }
+            else
+            {
+                Play();
+                offsetTimer.Stop();
+            }
+            Console.WriteLine("OnOffsetTimerTimeout");
             ReportBeat();
         }
     }
-
-    public void ReportBeat()
-    {
-        if (lastReportedBeat < songPositionInBeats)
-        {
-            if (measure > measures)
-            {
-                measure = 1;
-            }
-            EmitSignal(SignalName.Beat, songPositionInBeats);
-            EmitSignal(SignalName.Measure, measure);
-            lastReportedBeat = songPositionInBeats;
-            measure += 1;
-        }
-    }
-
-    public void BeatOffset(int num)
-    {
-        this.beatsBeforeStart = num;
-        offsetTimer.WaitTime = crotchet;
-        offsetTimer.Start();
-    }
-
-    public void OnOffsetTimerTimeout()
-    {
-        this.songPositionInBeats += 1;
-        if (this.songPositionInBeats < this.beatsBeforeStart - 1)
-        {
-            offsetTimer.Start();
-        }
-        else if (this.songPositionInBeats == this.beatsBeforeStart)
-        {
-            offsetTimer.WaitTime = offsetTimer.WaitTime -
-                                    (AudioServer.GetTimeToNextMix() + AudioServer.GetOutputLatency());
-            offsetTimer.Start();
-        }
-        else
-        {
-            Play();
-            offsetTimer.Stop();
-        }
-        ReportBeat();
-    }
-
-
 }
