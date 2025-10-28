1e240c9adb560f43d9bb722fdd378a4776266049
niko <91669604+qtniko@users.noreply.github.com>
2025-10-28 21:02:16 +0100

    mediapipeless controller (drs_controller_v2)

---
+++ controller/drs_controller_v2/__main__.py
@@ -0,0 +1,5 @@
+from drs_controller_v2.control.controller import Controller
+
+controller = Controller()
+
+controller.start()
---
+++ controller/drs_controller_v2/logic/foot_state.py
@@ -0,0 +1,33 @@
+class FootState:
+    def __init__(self, threshold):
+        self.state = "Idle"
+        self.x = 0
+        self.y = 0
+        self.speed_y = 0
+        self.threshold = threshold
+
+    def update(self, x, y, speed_y, time):
+        if self.state == "Idle":
+            if speed_y < -self.threshold:
+                self.state = "Up"
+
+        elif self.state == "Up":
+            if speed_y > self.threshold:
+                self.state = "Down"
+
+        elif self.state == "Down":
+            if speed_y < abs(self.threshold):
+                self.state = "Press"
+
+        elif self.state == "Press":
+            self.state = "Idle"
+
+        self.x = x
+        self.y = y
+        self.speed_y = speed_y
+        self.time = time
+
+    def __repr__(self):
+        return (
+            f"FootState(state={self.state}, x={self.x:.3f}, y_speed={self.speed_y:.3f})"
+        )
